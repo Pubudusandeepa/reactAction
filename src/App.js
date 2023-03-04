0f4d4f63f02edd548a686f782bab9d23c71987dc
Pubudusandeepa <ppsandeepa12@gmail.com>
2023-03-04 12:02:24 +0530

    gita

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Github Action for react Mighty rode runner</h2>
+        <h2>Github Action for react Mighty rode runner Github</h2>
       </header>
     </div>
   );
